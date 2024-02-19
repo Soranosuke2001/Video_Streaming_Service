823801aba1bca4a1377140d22c2758769a45a47c
Soranosuke2001 <sorazora1.4@gmail.com>
2024-02-19 14:13:09 -0800

    all services work but mysql db service

--- authentication/app.py
+++ authentication/app.py
@@ -64,4 +64,4 @@ def register():
 
 if __name__ == '__main__':
 
-  app.run(debug=True, port=app_config['app']['port'], host=app_config['app']['host'])
+  app.run(port=app_config['app']['port'], host=app_config['app']['host'])
